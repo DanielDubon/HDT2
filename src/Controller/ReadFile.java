be91908ac9cd2ed4725101cc331b88894c346de3
DanielDubon <dub22233@uvg.edu.gt>
2023-01-25 15:46:21 -0600

    changes to read file

--- src/Controller/ReadFile.java
+++ src/Controller/ReadFile.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ReadFile {
-    public static void main(String[] args) {
+    public static String ReadFile(){
         System.out.println("Ingrese la ruta del archivo ej C:\\ejemplos\\example1.txt");
         Scanner in = new Scanner(System.in);
         String fpath = in.nextLine();
@@ -13,14 +13,18 @@ public class ReadFile {
         try {
             File myObj = new File(fpath);
             Scanner myReader = new Scanner(myObj);
+            String data="";
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                data =data + myReader.nextLine();
+
             }
+            return data;
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+
     }
 }
