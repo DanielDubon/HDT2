f80234ea239c4060f115e8db8b2881d6e876030c
DanielDubon <dub22233@uvg.edu.gt>
2023-01-25 14:29:25 -0600

    Istack implemented

---
+++ src/Model/IStack.java
@@ -0,0 +1,18 @@
+package Model;
+
+/**
+ * @author MAAG
+ *
+ */
+public interface IStack<T> {
+
+    int count();
+
+    boolean isEmpty();
+
+    void push(T value);
+
+    T pull();
+
+    T peek();
+}
