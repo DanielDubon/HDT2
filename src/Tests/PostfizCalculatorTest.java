22099be292917d7a2334676f2cba45e912bc4dfc
DanielDubon <dub22233@uvg.edu.gt>
2023-01-27 19:41:06 -0600

    Tests done.

--- src/Tests/PostfizCalculatorTest.java
+++ src/Tests/PostfizCalculatorTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import Model.PostfizCalculator;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,22 +8,31 @@ import static org.junit.Assert.*;
 public class PostfizCalculatorTest {
 
     //Tests sketch
-
+    private PostfizCalculator calculator = new PostfizCalculator();
+    private int resultado;
     @Test
     public void suma() {
-        assertEquals(2,1,1);
-        //Test passed
+        resultado = calculator.suma(1,3);
+        assertEquals(4,resultado);
+
     }
 
     @Test
     public void resta() {
+        resultado = calculator.resta(4,2);
+        assertEquals(2,resultado);
+
     }
 
     @Test
     public void multiplicacion() {
+        resultado = calculator.multiplicacion(5,5);
+        assertEquals(25,resultado);
     }
 
     @Test
     public void division() {
+        resultado = calculator.division(10,2);
+        assertEquals(5,resultado);
     }
 }
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -16,75 +16,76 @@ public class Main {
         PostfizCalculator calculator = new PostfizCalculator();
         StackT<Integer> operandos = new StackT<Integer>(); //Stack handmade
         Scanner scan = new Scanner(System.in);
+        while (menu) {
+            //Menu settings
+            boolean exit = false;
+            System.out.println(" -----MENU----");
+            System.out.println("1.. Calcular resultado de expresión-->postfix");
+            System.out.println("2. Verificar pila");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scan.nextInt();
 
-        //Menu settings
-        boolean exit = false;
-        System.out.println(" -----MENU----");
-        System.out.println("1.. Calcular resultado de expresión-->postfix");
-        System.out.println("2. Verificar pila");
-        System.out.println("3. Salir");
-        System.out.print("Seleccione una opción: ");
-        int opcion = scan.nextInt();
+            //Menu
+            switch (opcion) {
+                case 1:
 
-        //Menu
-        switch (opcion) {
-            case 1:
+                    String expresion = ReadFile.read();
 
-                String expresion = ReadFile.read();
+                    System.out.println("GETTING POST FIX FORMAT");
+                    System.out.println(expresion);
+                    System.out.println("=======================");
 
-                System.out.println("GETTING POST FIX FORMAT");
-                System.out.println(expresion);
-                System.out.println("=======================");
-
-                ArrayList<String> items = calculator.getItems(expresion);
-                System.out.println(items.size());
-                for (String item : items) {
-                    System.out.println(item);
-                }
-                for (String item : items) {
-
-                    if (calculator.isOperator(item)) {
-
-                        int b = (int) operandos.pull();
-                        int a = (int) operandos.pull();
-
-                        int resultado = 0;
-                        switch (item) {
-                            case "+":
-                                resultado = calculator.suma(a, b);
-                                break;
-                            case "-":
-                                resultado = calculator.resta(a, b);
-                                break;
-                            case "*":
-                                resultado = calculator.multiplicacion(a, b);
-                                break;
-                            case "/":
-                                resultado = calculator.division(a, b);
-                                break;
-                        }
-                        operandos.push(resultado);
-                    } else {
-                        operandos.push(Integer.parseInt(item));
+                    ArrayList<String> items = calculator.getItems(expresion);
+                    System.out.println(items.size());
+                    for (String item : items) {
+                        System.out.println(item);
                     }
-                }
-                System.out.println("Resultado: " + operandos.peek());
-                break;
+                    for (String item : items) {
 
-            case 2:
-                if (calculator.isOneItem(operandos)) {
-                    System.out.println("La pila solo tiene un elemento");
-                } else {
-                    System.out.println("La pila tiene mas de un elemento");
-                }
-                break;
-            case 3:
-                exit = true;
-                menu = false;
-                break;
-            default:
-                System.out.println("Opcion no valida.");
-                break;
+                        if (calculator.isOperator(item)) {
+
+                            int b = (int) operandos.pull();
+                            int a = (int) operandos.pull();
+
+                            int resultado = 0;
+                            switch (item) {
+                                case "+":
+                                    resultado = calculator.suma(a, b);
+                                    break;
+                                case "-":
+                                    resultado = calculator.resta(a, b);
+                                    break;
+                                case "*":
+                                    resultado = calculator.multiplicacion(a, b);
+                                    break;
+                                case "/":
+                                    resultado = calculator.division(a, b);
+                                    break;
+                            }
+                            operandos.push(resultado);
+                        } else {
+                            operandos.push(Integer.parseInt(item));
+                        }
+                    }
+                    System.out.println("Resultado: " + operandos.peek());
+                    break;
+
+                case 2:
+                    if (calculator.isOneItem(operandos)) {
+                        System.out.println("La pila solo tiene un elemento");
+                    } else {
+                        System.out.println("La pila tiene mas de un elemento");
+                    }
+                    break;
+                case 3:
+                    exit = true;
+                    menu = false;
+                    break;
+                default:
+                    System.out.println("Opcion no valida.");
+                    break;
+            }
         }
     }
 
